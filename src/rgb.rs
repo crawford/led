9c86e9c648aa9af679ff556611d5863c0ad895b5
Alex Crawford <alex@acrawford.com>
2020-02-02 05:54:53 -0800

    STASH

--- src/rgb.rs
+++ src/rgb.rs
@@ -19,7 +19,7 @@
 //! # Examples
 //!
 //! ```
-//! # use embedded_hal::digital::OutputPin;
+//! # use embedded_hal::digital::v2::OutputPin;
 //! #
 //! # struct Pin {}
 //! #
@@ -33,15 +33,15 @@
 //! # let b = Pin{};
 //! #
 //! use led::LED;
-//! use led::rgb::{Color, CommonAnodeLED};
+//! use led::rgb::{DiscreteColor, CommonAnodeLED};
 //!
 //! let mut led = CommonAnodeLED::new(r, g, b);
 //!
-//! led.set(Color::Green);
+//! led.set(DiscreteColor::Green);
 //! ```
 //!
 //! ```
-//! # use embedded_hal::digital::OutputPin;
+//! # use embedded_hal::digital::v2::OutputPin;
 //! #
 //! # struct Pin {}
 //! #
@@ -58,7 +58,7 @@
 //! # let b2 = Pin{};
 //! #
 //! use led::LED;
-//! use led::rgb::{Color, CommonAnodeLED, CommonCathodeLED, RGB};
+//! use led::rgb::{DiscreteColor, CommonAnodeLED, CommonCathodeLED, RGB};
 //!
 //! let mut leds: [&mut RGB; 2] = [
 //!     &mut CommonAnodeLED::new(r1, g1, b1),
@@ -66,16 +66,23 @@
 //! ];
 //!
 //! for led in leds.iter_mut() {
-//!     led.set(Color::Red)
+//!     led.set(DiscreteColor::Red)
 //! }
 //! ```
 
 use core::marker::PhantomData;
-use embedded_hal::digital::OutputPin;
+use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::PwmPin;
+
+pub struct Color {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
+}
 
 /// The set of primary colors and secondary colors that can be created by an RGB LED along with
 /// black and white.
-pub enum Color {
+pub enum DiscreteColor {
     Red,
     Green,
     Blue,
@@ -85,6 +92,12 @@ pub enum Color {
     White,
     Black,
 }
+
+/// A common anode LED.
+pub type CommonAnodeDiscreteLED<R, G, B> = DiscreteLED<CommonAnode, R, G, B>;
+
+/// A common cathode LED.
+pub type CommonCathodeDiscreteLED<R, G, B> = DiscreteLED<CommonCathode, R, G, B>;
 
 /// A common anode LED.
 pub type CommonAnodeLED<R, G, B> = LED<CommonAnode, R, G, B>;
@@ -97,7 +110,7 @@ pub type CommonCathodeLED<R, G, B> = LED<CommonCathode, R, G, B>;
 /// # Examples
 ///
 /// ```
-/// # use embedded_hal::digital::OutputPin;
+/// # use embedded_hal::digital::v2::OutputPin;
 /// #
 /// # struct Pin {}
 /// #
@@ -113,7 +126,8 @@ pub type CommonCathodeLED<R, G, B> = LED<CommonCathode, R, G, B>;
 /// # let g2 = Pin{};
 /// # let b2 = Pin{};
 /// #
-/// use led::rgb::{Color, CommonAnodeLED, CommonCathodeLED, RGB};
+/// ALEX (order)
+/// use led::rgb::{DiscreteColor, CommonAnodeLED, CommonCathodeLED, RGB};
 ///
 /// let mut leds: [&mut RGB; 2] = [
 ///     &mut CommonAnodeLED::new(r1, g1, b1),
@@ -121,15 +135,104 @@ pub type CommonCathodeLED<R, G, B> = LED<CommonCathode, R, G, B>;
 /// ];
 ///
 /// for led in leds.iter_mut() {
-///     led.set(Color::Red)
+///     led.set(DiscreteColor::Red)
 /// }
 /// ```
+pub trait DiscreteRGB: crate::LED<Input = DiscreteColor> {}
+impl<L> DiscreteRGB for L where L: crate::LED<Input = DiscreteColor> {}
+
 pub trait RGB: crate::LED<Input = Color> {}
 impl<L> RGB for L where L: crate::LED<Input = Color> {}
 
 /// An RGB LED
 ///
-/// The RGB LED is represented by three owned instances of `embedded_hal::digital::OutputPin` and a
+/// The RGB LED is represented by three owned instances of `embedded_hal::digital::v2::OutputPin` and a
+/// polarity (common anode or common cathode). Because the outputs are binary, only eight colors
+/// can be presented: primary colors, secondary colors, white, and black.
+pub struct DiscreteLED<C, R, G, B> {
+    common: PhantomData<C>,
+    red: R,
+    green: G,
+    blue: B,
+}
+
+impl<C, R, G, B> DiscreteLED<C, R, G, B>
+where
+    C: Common,
+    R: OutputPin,
+    G: OutputPin,
+    B: OutputPin,
+{
+    /// Creates a new RGB LED given three GPIOs.
+    pub fn new(red: R, green: G, blue: B) -> DiscreteLED<C, R, G, B> {
+        DiscreteLED {
+            common: PhantomData,
+            red,
+            green,
+            blue,
+        }
+    }
+}
+
+impl<C, R, G, B> crate::LED for DiscreteLED<C, R, G, B>
+where
+    C: Common,
+    R: OutputPin,
+    G: OutputPin,
+    B: OutputPin,
+{
+    type Input = DiscreteColor;
+
+    /// Sets the RGB LED to the specified color.
+    fn set(&mut self, color: DiscreteColor) {
+        match color {
+            DiscreteColor::Red => {
+                C::enable(&mut self.red);
+                C::disable(&mut self.green);
+                C::disable(&mut self.blue);
+            }
+            DiscreteColor::Green => {
+                C::disable(&mut self.red);
+                C::enable(&mut self.green);
+                C::disable(&mut self.blue);
+            }
+            DiscreteColor::Blue => {
+                C::disable(&mut self.red);
+                C::disable(&mut self.green);
+                C::enable(&mut self.blue);
+            }
+            DiscreteColor::Yellow => {
+                C::enable(&mut self.red);
+                C::enable(&mut self.green);
+                C::disable(&mut self.blue);
+            }
+            DiscreteColor::Cyan => {
+                C::disable(&mut self.red);
+                C::enable(&mut self.green);
+                C::enable(&mut self.blue);
+            }
+            DiscreteColor::Magenta => {
+                C::enable(&mut self.red);
+                C::disable(&mut self.green);
+                C::enable(&mut self.blue);
+            }
+            DiscreteColor::White => {
+                C::enable(&mut self.red);
+                C::enable(&mut self.green);
+                C::enable(&mut self.blue);
+            }
+            DiscreteColor::Black => {
+                C::disable(&mut self.red);
+                C::disable(&mut self.green);
+                C::disable(&mut self.blue);
+            }
+        }
+    }
+}
+
+/// An RGB LED
+///
+/// The RGB LED is represented by three owned instances of `embedded_hal::digital::v2::OutputPin` and a
 /// polarity (common anode or common cathode). Because the outputs are binary, only eight colors
 /// can be presented: primary colors, secondary colors, white, and black.
 pub struct LED<C, R, G, B> {
@@ -142,9 +245,9 @@ pub struct LED<C, R, G, B> {
 impl<C, R, G, B> LED<C, R, G, B>
 where
     C: Common,
-    R: OutputPin,
-    G: OutputPin,
-    B: OutputPin,
+    R: PwmPin,
+    G: PwmPin,
+    B: PwmPin,
 {
     /// Creates a new RGB LED given three GPIOs.
     pub fn new(red: R, green: G, blue: B) -> LED<C, R, G, B> {
@@ -157,59 +260,21 @@ where
     }
 }
 
-impl<C, R, G, B> crate::LED for LED<C, R, G, B>
+impl<C, R, G, B, D> crate::LED for LED<C, R, G, B>
 where
     C: Common,
-    R: OutputPin,
-    G: OutputPin,
-    B: OutputPin,
+    R: PwmPin<Duty = D>,
+    G: PwmPin<Duty = D>,
+    B: PwmPin<Duty = D>,
+    D: From<u8>,
 {
     type Input = Color;
 
     /// Sets the RGB LED to the specified color.
     fn set(&mut self, color: Color) {
-        match color {
-            Color::Red => {
-                C::enable(&mut self.red);
-                C::disable(&mut self.green);
-                C::disable(&mut self.blue);
-            }
-            Color::Green => {
-                C::disable(&mut self.red);
-                C::enable(&mut self.green);
-                C::disable(&mut self.blue);
-            }
-            Color::Blue => {
-                C::disable(&mut self.red);
-                C::disable(&mut self.green);
-                C::enable(&mut self.blue);
-            }
-            Color::Yellow => {
-                C::enable(&mut self.red);
-                C::enable(&mut self.green);
-                C::disable(&mut self.blue);
-            }
-            Color::Cyan => {
-                C::disable(&mut self.red);
-                C::enable(&mut self.green);
-                C::enable(&mut self.blue);
-            }
-            Color::Magenta => {
-                C::enable(&mut self.red);
-                C::disable(&mut self.green);
-                C::enable(&mut self.blue);
-            }
-            Color::White => {
-                C::enable(&mut self.red);
-                C::enable(&mut self.green);
-                C::enable(&mut self.blue);
-            }
-            Color::Black => {
-                C::disable(&mut self.red);
-                C::disable(&mut self.green);
-                C::disable(&mut self.blue);
-            }
-        }
+        self.red.set_duty(color.red.into());
+        self.green.set_duty(color.green.into());
+        self.blue.set_duty(color.blue.into());
     }
 }
 
@@ -227,7 +292,7 @@ pub trait Common {
 /// # Examples
 ///
 /// ```
-/// # use embedded_hal::digital::OutputPin;
+/// # use embedded_hal::digital::v2::OutputPin;
 /// #
 /// # struct Pin {}
 /// #
@@ -253,7 +318,7 @@ pub struct CommonAnode {
 /// # Examples
 ///
 /// ```
-/// # use embedded_hal::digital::OutputPin;
+/// # use embedded_hal::digital::v2::OutputPin;
 /// #
 /// # struct Pin {}
 /// #
